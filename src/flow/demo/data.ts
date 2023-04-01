ebfaec84fc9e6b7eab551cc682a42aae3cd68e70
grh-gitee <3494242908@qq.com>
2023-04-01 17:53:51 +0800

    feat: 组件开发

--- src/flow/demo/data.ts
+++ src/flow/demo/data.ts
@@ -4,9 +4,12 @@ import { FlowChartNode } from '..'
 function flowAdapter(nodes: FlowChartNode[][]) {
 	const newNodes: FlowChartNode[][] = [...nodes]
 
+	// 行
 	for (let i = 0; i < nodes.length; i++) {
-		const row: FlowChartNode[] = nodes[i]
+		const row: FlowChartNode[] = newNodes[i]
 		if (!isEffectArray(row)) continue;
+
+		// 列
 		for (let j = 0; j < row.length; j++) {
 			const item: FlowChartNode = row[j]
 			const { id, link } = item
@@ -20,34 +23,77 @@ function flowAdapter(nodes: FlowChartNode[][]) {
 			const nextColId = row[j + 1]?.id
 			if (!isUndefined(nextColId) && links.includes(nextColId)) {
 				item.rightArrow = true
-				links = links.filter(_id => nextColId != _id)
+				links = links.filter(_id => nextColId !== _id)
 			}
 			// 下一
 			const nextRowId = isEffectArray(nodes[i + 1]) ? nodes[i + 1][j]?.id : undefined
 			if (!isUndefined(nextRowId) && links.includes(nextRowId)) {
 				item.bottomArrow = true
-				links = links.filter(_id => nextRowId != _id)
+				links = links.filter(_id => nextRowId !== _id)
 			}
 			// 斜一
 			const nextRowNextColId = isEffectArray(nodes[i + 1]) ? nodes[i + 1][j + 1]?.id : undefined
 			if (!isUndefined(nextRowNextColId) && links.includes(nextRowNextColId)) {
 				item.bottomRightArrow = true
-				links = links.filter(_id => nextRowNextColId != _id)
+				links = links.filter(_id => nextRowNextColId !== _id)
+			}
+
+			if (links.length === 0) continue;
+			if (i + 1 === nodes.length) continue;
+
+			// 斜X (下一行)
+			const nextRow: FlowChartNode[] = isEffectArray(nodes[i + 1]) ? nodes[i + 1] : []
+			if (isEffectArray(nextRow)) {
+				for (let x = j + 1; x < nextRow.length; x++) {
+					const xRowId = nextRow[x]?.id
+					if (isUndefined(xRowId)) continue;
+					if (links.includes(xRowId)) {
+
+						links = links.filter(_id => xRowId !== _id)
+
+						if (isUndefined(newNodes[i][x - 1])) continue;
+						if (isUndefined(newNodes[i][x - 1].bottomArrowCover)) newNodes[i][x - 1].bottomArrowCover = {};
+						if (isUndefined(newNodes[i][x - 1].bottomRightArrowCover)) newNodes[i][x - 1].bottomRightArrowCover = {};
+						if (isUndefined(item.bottomRightArrowCover)) item.bottomRightArrowCover = {}
+						if (isUndefined(item.bottomRightArrowCover.style)) item.bottomRightArrowCover.style = {}
+
+							; (newNodes[i][x - 1].bottomArrowCover as any).hidden = true
+							; (newNodes[i][x - 1].bottomRightArrowCover as any).hidden = true
+						item.bottomRightArrow = true
+						item.bottomRightArrowCover.style.gridColumn = 'auto / span 3'
+						break;
+					}
+				}
 			}
 
 			if (links.length === 0) continue;
 
-			// 斜X (下一行)
-			// const nextRowNextXColId = isEffectArray(nodes[i + 1]) ? nodes[i + 1][j + 1]?.id : undefined
-			// if (!isUndefined(nextRowNextXColId) && links.includes(nextRowNextXColId)) {
-			// 	item.bottomRightArrow = true
-			// }
-
 			// 斜X (右一列)
-			const nextXRowNextColId = isEffectArray(nodes[i + 1]) ? nodes[i + 1][j + 1]?.id : undefined
-			if (!isUndefined(nextXRowNextColId) && links.includes(nextXRowNextColId)) {
-				// item.bottomRightArrow = true
+			const nextColIds: (string | undefined)[] = newNodes.map(row => row[j + 1]?.id).filter(Boolean)
+			if (isEffectArray(nextColIds)) {
+
+				for (let y = 0; y < nextColIds.length; y++) {
+					const nextColId = nextColIds[y]
+					if (!(nextColId && links.includes(nextColId))) continue;
+					links = links.filter(_id => nextColId !== _id)
+
+					if(isUndefined(newNodes[y-1][j])) continue;
+					if(isUndefined(item.bottomRightArrowCover)) item.bottomRightArrowCover ={}
+					if(isUndefined(item.bottomRightArrowCover.style)) item.bottomRightArrowCover.style ={}
+
+					if(isUndefined(newNodes[y-1][j].rightArrowCover)) newNodes[y-1][j].rightArrowCover = {}
+					if(isUndefined(newNodes[y-1][j].bottomRightArrowCover)) newNodes[y-1][j].bottomRightArrowCover = {}
+
+					item.bottomRightArrow = true
+					item.bottomRightArrowCover.style.gridRow = 'auto / span 3'
+					;(newNodes[y-1][j].rightArrowCover as any).hidden = true;
+					;(newNodes[y-1][j].bottomRightArrowCover as any).hidden = true;
+					break;
+				}
+
+				continue;
 			}
+		
 		}
 	}
 
@@ -57,68 +103,37 @@ function flowAdapter(nodes: FlowChartNode[][]) {
 export const nodes: FlowChartNode[][] = flowAdapter([
 	[
 		{
-			id: '1', label: 'Node 1', link: '2', status: 'operable',
-			rightArrowCover: {
-				dottedLine: true
-			},
-			bottomRightArrow: true,
-			bottomRightArrowCover: {
-				style: {
-					gridRow: 'auto / span 3'
-				}
-			}
+			id: '1', label: 'Node 1', link: ['2', '8'], status: 'operable',
+			rightArrowCover: { dottedLine: true },
 		},
 		{
 			id: '2',
 			label: 'Node 2',
 			status: 'error',
-			link: ['3', '5'],
+			link: ['3', '4', '5'],
 		},
 		{
 			id: '3',
 			label: 'node 3',
 			status: 'finish',
 			width: 200,
-			link: ['5', '6'],
+			link: ['6'],
 		},
-		{}
+		{},
+		{},
 	],
 	[
-		{
-			rightArrowCover: {
-				hidden: true
-			},
-			bottomRightArrowCover: {
-				hidden: true
-			}
-		},
-		{
-			id: '4', label: 'Node 4', link: '8',
-			bottomRightArrow: true,
-			bottomRightArrowCover: {
-				style: {
-					gridColumn: 'auto / 3 span'
-				}
-			}
-		},
-		{
-			id: '5', label: 'Node 5', link: '6',
-			bottomArrowCover: {
-				hidden: true,
-			},
-			bottomRightArrowCover: {
-				hidden: true,
-			}
-		},
-		{ id: '6', label: 'Node 6', link: '7' },
+		{ },
+		{ id: '4', label: 'Node 4', link: ['8', '5', '10'], },
+		{ id: '5', label: 'Node 5', link: '6' },
+		{ id: '6', label: 'Node 6' },
+		{},
 	],
 	[
 		{ id: '7', label: 'Node 7', link: '8' },
 		{ id: '8', label: 'Node 8', link: '9' },
 		{ id: '9', label: 'Node 9', link: '10' },
-		{ id: '10', label: 'Node 10', link: '10' },
+		{ id: '10', label: 'Node 10', link: '11' },
+		{ id: '11', label: 'Node 11' },
 	]
 ])
-
-
-
--- src/flow/demo/index.tsx
+++ src/flow/demo/index.tsx
@@ -6,16 +6,15 @@ import { nodes } from './data'
 export default () => {
 	return <Container columns={1}>
 		<Unit title="Flow">
-			<FlowChart nodes={nodes} rowGap={65} nodeWidth={200} />
-			<div>1. 不支持线交叉</div>
+			<div>1. 不支持线交叉, 同一点两个线连接</div>
 			<div>2. 不支持跨节点连线</div>
 			<div>3. 只允许箭头从左往(斜)右 </div>
+			<div>4. 只支持跨一行或一列连线 </div>
+			<div>5. 由于是网格布局, 局限性较大</div>
+			<FlowChart nodes={nodes} rowGap={65} nodeWidth={200} />
 		</Unit>
 		<Unit title="Flow">
 			<FlowChart nodes={nodes} rowGap={65} />
-			<div>1. 不支持线交叉</div>
-			<div>2. 不支持跨节点连线</div>
-			<div>3. 只允许箭头从左往(斜)右 </div>
 		</Unit>
 	</Container>
 };
