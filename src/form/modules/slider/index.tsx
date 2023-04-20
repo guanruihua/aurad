dbfe7ad9a02796b6aeb7c79e8bf97acfb06a1852
grh-gitee <3494242908@qq.com>
2023-04-20 23:10:02 +0800

    feat: 调研技术

--- src/form/modules/slider/index.tsx
+++ src/form/modules/slider/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable*/
 import React from "react"
 import { ComponentProps } from '@/assets'
+import './index.less'
 
 export interface SliderProps extends ComponentProps {
 
@@ -26,5 +27,54 @@ export function Slider(props: SliderProps) {
 		// defaultValue={props.defaultValue}
 		// onChange={handleChange}
 		/>
+	);
+}
+
+export function SliderDev(props: SliderProps) {
+
+	return (
+		<div className="au-slider">
+			{/* <div className="au-slider-rail" ></div> */}
+			<div className="au-slider-step" >
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+			<div className="au-slider-handle" >
+				<span className="start"></span>
+				<span className="end"
+					style={{
+						cursor: 'grab'
+					}}
+					draggable
+					onDragStart={(e) => {
+						// const target: any = e.target
+						// e.dataTransfer.effectAllowed = 'none';
+						// e.dataTransfer.effectAllowed = 'all';
+						// target.classList.add('dragging');
+						console.log('start', e)
+					}}
+					onDragOver={(e: any) => {
+						// e.dataTransfer.effectAllowed = 'all';
+						// e.target.classList.add('dragging');
+						// console.log('over', e)
+						e.preventDefault();
+					}}
+					// onDragLeave={(e: any) => {
+					// 	e.dataTransfer.effectAllowed = 'all';
+					// 	e.target.classList.add('dragging');
+					// }}
+					onDrop={(e) => {
+						e.preventDefault()
+
+					}}
+					onDragEnd={(e: any) => {
+						console.log('end', e)
+						// e.target.classList.remove("dragging");
+					}}>
+				</span>
+			</div>
+		</div>
 	);
 }
