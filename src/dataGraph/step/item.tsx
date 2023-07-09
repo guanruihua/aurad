6b352229619c1dcc9b5c28fa6724c98b0103633a
grh-gitee <3494242908@qq.com>
2023-07-09 21:50:44 +0800

    dev: 开发功能

--- src/dataGraph/step/item.tsx
+++ src/dataGraph/step/item.tsx
@@ -2,21 +2,43 @@ import { ComponentProps } from '@/assets'
 import React from 'react'
 import { StepStatus } from './type'
 import { classNames } from 'harpe'
+import { Icon } from '@/icon'
+import { isEmpty } from 'asura-eye'
 
-
-export interface StepItemProps extends ComponentProps {
-	first?: boolean
-	status?: StepStatus
+const IconStatus = {
+	wait: <Icon type='block' />,
+	process: <div style={{
+		width: 30,
+		height: 30,
+		background: 'var(--disabled-color)',
+		borderRadius: '50%',
+		border: 'none',
+	}} />,
+	finish: <Icon type='yes' />,
+	error: <Icon type='no' />,
 }
 
+export interface StepItemProps extends ComponentProps {
+	status?: StepStatus
+	__first__?: boolean
+}
+
+const prefix = 'au-step-item'
+const Prefix = (name?: string) => `${prefix}${isEmpty(name) ? '' : ('-' + name)}`
+
 export function Item(props: StepItemProps) {
-	const { first = false, status, children } = props
+	const { status = 'process', __first__ = false, children } = props
 	return (
 		<>
-			{!first && <span className='au-step-item-line' />}
-			<label className={classNames(status, 'au-step-item')}>
-				{children}
-			</label>
+			{!__first__ && <div className={classNames(Prefix('line'))} />}
+			<div className={classNames(Prefix())}>
+				<div className={Prefix('icon')}>
+					{IconStatus[status]}
+				</div>
+				<div className={Prefix('content')}>
+					{children}
+				</div>
+			</div>
 		</>
 	)
 }
