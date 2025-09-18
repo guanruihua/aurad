d02bfeed73689476bbf9ca21aca9eec1f37f434e
guanruihua <2116047353@qq.com>
2025-09-18 15:45:07 +0800

    chore: 1.11.7

--- src/echart/index.tsx
+++ src/echart/index.tsx
@@ -22,17 +22,19 @@ export type ChartProps = {
 
 export function Chart(props: ChartProps) {
   const { className, style, options, init, initCharFn, ...rest } = props
-  let chartDom: any
+  const chartDom = React.useRef<HTMLDivElement>(null)
   let myChart: any
 
   React.useEffect(() => {
+    if (!chartDom.current) return
+
     try {
       // console.log(echarts, chartDom, options)
       if (chartDom) {
         //判断是否已存在实例
-        myChart = echarts.getInstanceByDom(chartDom)
+        myChart = echarts.getInstanceByDom(chartDom.current)
         if (!myChart) {
-          myChart = echarts.init(chartDom)
+          myChart = echarts.init(chartDom.current)
         }
         //回传实例
         if (init) init(myChart)
@@ -51,13 +53,17 @@ export function Chart(props: ChartProps) {
     } catch (error) {
       console.log(error)
     }
-  }, [options])
+  }, [options, chartDom.current])
 
   return (
     <div
       className={classNames(className)}
-      style={style}
-      ref={(e: any) => (chartDom = e)}
+      ref={chartDom}
+      style={{
+        height: '100%',
+        width: '100%',
+        ...style,
+      }}
       {...rest}
     />
   )
